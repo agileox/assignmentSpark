3ed9335a037c937b7af011b562cfbf8a0e00f787
Agileox <agileox1979@gmail.com>
2024-10-21 16:06:36 +0800

    update Q2

--- Q2.scala
+++ Q2.scala
@@ -1,3 +1,4 @@
+//library required for spark scala
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
@@ -8,7 +9,7 @@ val spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
 // Read CSV file for flights and cast data types
 val flightsDF = spark.read
   .option("header", "true")
-  .csv("/home/agileox/Project/assignmentSpark/data/flightData.csv")
+  .csv("/home/agileox/Project/assignmentSpark/data/flightData.csv") //read csv data location
   .withColumn("passengerId", col("passengerId").cast(IntegerType))
   .withColumn("flightId", col("flightId").cast(IntegerType))
   .withColumn("date", col("date").cast(DateType))
@@ -16,29 +17,26 @@ val flightsDF = spark.read
 // Read the passengers CSV file
 val passengersDF = spark.read
   .option("header", "true")
-  .csv("/home/agileox/Project/assignmentSpark/data/passengers.csv")
+  .csv("/home/agileox/Project/assignmentSpark/data/passengers.csv") //read csv data location
   .withColumn("passengerId", col("passengerId").cast(IntegerType)) // Ensure passengerId is of the correct type
 
 // Join the flights DataFrame with the passengers DataFrame
-val joinedDF = flightsDF.join(passengersDF, Seq("passengerId"), "inner") // Inner join on passengerId
+val joinedDF = flightsDF.join(passengersDF, Seq("passengerId"), "inner") // Inner join on passengerId - Join both table function
 
 // Group by passengerId, firstName, and lastName, and count occurrences
 val countByPassengerDF = joinedDF.groupBy("passengerId", "firstName", "lastName")
-  .agg(count("*").alias("flightCount")) // Count the number of rows for each group
+  .agg(count("*").alias("Number_of_Flights")) // Count the number of rows for each group
   .orderBy(desc("flightCount")) // Order by flightCount in descending order
 
 // Show the grouped and counted data
-countByPassengerDF.show()
+//countByPassengerDF.show() //to show output in terminal - remark at the moment
 
 // Optionally, save the count output to a file
 countByPassengerDF.coalesce(1)
   .write
   .option("header", "true") // Write the header
-  .csv("/home/agileox/Project/assignmentSpark/output/countByPassenger.csv")
+  .csv("/home/agileox/Project/assignmentSpark/output/Most_Frequent_Fliers") // output location
 
 // Stop the Spark session
-spark.stop()
-//df.unpersist() // Unpersist if cached
-//spark.getPersistentRDDs.values.foreach(_.unpersist()) // Clear all cached RDDs
 spark.stop() // Stop the Spark context
 System.gc() // Suggest garbage collection
