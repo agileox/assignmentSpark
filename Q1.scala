9cfc580e16216bbf30388f4d895ef023aa7e0f5c
Agileox <agileox1979@gmail.com>
2024-10-21 16:02:41 +0800

    update

--- Q1.scala
+++ Q1.scala
@@ -1,3 +1,4 @@
+// the library required byt the code
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
@@ -5,7 +6,7 @@ import org.apache.spark.sql.types._
 // Create Spark Session
 val spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
 
-// Read CSV file and cast data types
+// Read CSV file and cast data types as it read the csv as text file
 val df = spark.read
   .option("header", "true")
   .csv("/home/agileox/Project/assignmentSpark/data/flightData.csv")
@@ -13,23 +14,20 @@ val df = spark.read
   .withColumn("flightId", col("flightId").cast(IntegerType))
   .withColumn("date", col("date").cast(DateType))
 
-// Extract year and month, and group by them
-val flightCountByMonthDF = df.groupBy(month(col("date")).alias("month"))
-  .agg(count("flightId").alias("uniqueFlightCount"))
-  .orderBy(col("month")) // Order by month
+// Extract year and month, and group by them -> this is where the date being converted to month
+val flightCountByMonthDF = df.groupBy(month(col("date")).alias("Months")) // the conversion function happen
+  .agg(count("flightId").alias("Number_Of_Flights"))
+  .orderBy(col("Months")) // Order by month
 
 // Show the grouped data
-flightCountByMonthDF.show()
+// flightCountByMonthDF.show() // for showing the data in terminal but will remark for keeping it into csv file output
 
 // Save the output to a file (e.g., in CSV format)
 flightCountByMonthDF.coalesce(1)
   .write
   .option("header", "true") // Write the header
-  .csv("/home/agileox/Project/assignmentSpark/output/flightCountByMonth.csv")
+  .csv("/home/agileox/Project/assignmentSpark/output/No_Of_Flight_Each_Months") //output file location
 
 // Stop the Spark session
-spark.stop()
-//df.unpersist() // Unpersist if cached
-//spark.getPersistentRDDs.values.foreach(_.unpersist()) // Clear all cached RDDs
 spark.stop() // Stop the Spark context
 System.gc() // Suggest garbage collection
